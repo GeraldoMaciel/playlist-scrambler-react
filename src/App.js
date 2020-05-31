851dd172520ca002ffc3ff05db5a0bbab0d6a78b
Geraldo Santos <geraldosantosneto@gmail.com>
2020-05-31 21:37:52 +0200

    improving the list of playlists

--- src/App.js
+++ src/App.js
@@ -1,258 +1,263 @@
 import React, { Component } from "react";
-import Button from 'react-bootstrap/Button';
-import Table from './Table.js';
-import Player from './Player.js';
-
-
+import Button from "react-bootstrap/Button";
+import Fade from "react-bootstrap/Fade";
+import Table from "./Table.js";
+import Player from "./Player.js";
 
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios';
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 
-
-
-const authEndpoint = 'https://accounts.spotify.com/authorize';
+const authEndpoint = "https://accounts.spotify.com/authorize";
 
 const clientId = "78fffce2862d4584bcb9eb68d18fbad1";
 const redirectUri = "http://localhost:3000";
 const scopes = [
-	"user-read-currently-playing",
-	"user-read-playback-state",
-	"user-modify-playback-state",
-	"streaming",
-	"user-read-email",
-	"user-read-private",
-	"user-library-read",
-	"user-library-modify",
-	"playlist-read-private",
-	"playlist-read-collaborative"
+  "user-read-currently-playing",
+  "user-read-playback-state",
+  "user-modify-playback-state",
+  "streaming",
+  "user-read-email",
+  "user-read-private",
+  "user-library-read",
+  "user-library-modify",
+  "playlist-read-private",
+  "playlist-read-collaborative",
 ];
 
 const PLAYLIST_SIZE = 10;
 
 // Get the hash of the url
 const hashmap = window.location.hash
-	.substring(1)
-	.split("&")
-	.reduce(function (initial, item) {
-		if (item) {
-			var parts = item.split("=");
-			initial[parts[0]] = decodeURIComponent(parts[1]);
-		}
-		return initial;
-	}, {});
+  .substring(1)
+  .split("&")
+  .reduce(function (initial, item) {
+    if (item) {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+    }
+    return initial;
+  }, {});
 
 window.location.hash = "";
 
-
-
-
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
 
-	constructor(props) {
-		super(props);
-		this.state = { };
+    this.fetchUserData = this.fetchUserData.bind(this);
+    this.fetchPlaylists = this.fetchPlaylists.bind(this);
+    this.setInitialList = this.setInitialList.bind(this);
+    this.generateRandomizedPlaylist = this.generateRandomizedPlaylist.bind(
+      this
+    );
+    this.selectPlayList = this.selectPlayList.bind(this);
+    this.spreadFunction = this.spreadFunction.bind(this);
+    this.playSomeMusic = this.playSomeMusic.bind(this);
 
-		
+    window.onSpotifyWebPlaybackSDKReady = () => {
+      const token = hashmap.access_token;
+      this.player = new window.Spotify.Player({
+        name: "Playlist Scrambler Player",
+        getOAuthToken: (cb) => {
+          cb(token);
+        },
+      });
 
-		this.fetchUserData = this.fetchUserData.bind(this);
-		this.fetchPlaylists = this.fetchPlaylists.bind(this);
-		this.setInitialList = this.setInitialList.bind(this);
-		this.generateRandomizedPlaylist = this.generateRandomizedPlaylist.bind(this);
-		this.selectPlayList = this.selectPlayList.bind(this);
-		this.spreadFunction = this.spreadFunction.bind(this);
-		this.playSomeMusic = this.playSomeMusic.bind(this);
-		
+      // Error handling
+      this.player.addListener("initialization_error", ({ message }) => {
+        console.error(message);
+      });
+      this.player.addListener("authentication_error", ({ message }) => {
+        console.error(message);
+      });
+      this.player.addListener("account_error", ({ message }) => {
+        console.error(message);
+      });
+      this.player.addListener("playback_error", ({ message }) => {
+        console.error(message);
+      });
 
-		window.onSpotifyWebPlaybackSDKReady = () => {
-			const token = hashmap.access_token;
-			this.player = new window.Spotify.Player({
-			  name: 'Playlist Scrambler Player',
-			  getOAuthToken: cb => { cb(token); }
-			});
-		  
-			// Error handling
-			this.player.addListener('initialization_error', ({ message }) => { console.error(message); });
-			this.player.addListener('authentication_error', ({ message }) => { console.error(message); });
-			this.player.addListener('account_error', ({ message }) => { console.error(message); });
-			this.player.addListener('playback_error', ({ message }) => { console.error(message); });
-		  
-			// Playback status updates
-			this.player.addListener('player_state_changed', state => { this.setState({playerState : state}) });
-		  
-			// Ready
-			this.player.addListener('ready', ({ device_id }) => {
-			  this.deviceId = device_id;
-			  console.log('Ready with Device ID', device_id);
-			});
-		  
-			// Not Ready
-			this.player.addListener('not_ready', ({ device_id }) => {
-			  console.log('Device ID has gone offline', device_id);
-			});
-		  
-			// Connect to the player!
-			this.player.connect();
-		  };
+      // Playback status updates
+      this.player.addListener("player_state_changed", (state) => {
+        this.setState({ playerState: state });
+      });
 
+      // Ready
+      this.player.addListener("ready", ({ device_id }) => {
+        this.deviceId = device_id;
+        console.log("Ready with Device ID", device_id);
+      });
 
-	}
+      // Not Ready
+      this.player.addListener("not_ready", ({ device_id }) => {
+        console.log("Device ID has gone offline", device_id);
+      });
 
+      // Connect to the player!
+      this.player.connect();
+    };
+  }
 
+  componentDidMount() {
+    // Set token
+    let _token = hashmap.access_token;
+    if (_token) {
+      axios.defaults.headers.common["Authorization"] = "Bearer " + _token;
+      this.token = _token;
+      this.fetchUserData(_token);
+    } else {
+      window.location.href = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+        "%20"
+      )}&response_type=token`;
+    }
+  }
 
-	componentDidMount() {
-		// Set token
-		let _token = hashmap.access_token;
-		if (_token) {
+  fetchUserData(token) {
+    axios("https://api.spotify.com/v1/me")
+      .then((result) => this.fetchPlaylists(result.data.id))
+      .catch((error) => error);
+  }
 
-			axios.defaults.headers.common['Authorization'] = 'Bearer ' + _token;
-			this.token = _token;
-			this.fetchUserData(_token);
+  fetchPlaylists(userid) {
+    axios(`https://api.spotify.com/v1/users/${userid}/playlists?limit=50`)
+      .then((result) => this.setInitialList(result.data.items))
+      .catch((error) => error);
+  }
 
-		} else {
-			window.location.href = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token`;
-		}
+  setInitialList(items) {
+    let playListArray = items.map(({ id, name, tracks, images }) => ({
+      id: id,
+      name: name,
+      tracksCount: tracks.total,
+      image: images[0],
+      checked: false,
+    }));
+    this.setState({ playListArray });
+  }
 
-	}
+  selectPlayList(playList) {
+    playList.checked = !playList.checked;
+    const selectedPlaylistArray = this.state.playListArray.filter(
+      (playlist) => playlist.checked
+    );
+    this.setState({ selectedPlaylistArray });
+  }
 
-	fetchUserData(token) {
-		axios("https://api.spotify.com/v1/me")
-			.then(result => this.fetchPlaylists(result.data.id))
-			.catch(error => error);
-	}
+  generateRandomizedPlaylist() {
+    const mapPlaylistTrack = new Map();
+    let trackCount = 0;
 
-	fetchPlaylists(userid) {
-		axios(`https://api.spotify.com/v1/users/${userid}/playlists?limit=50`)
-			.then(result => this.setInitialList(result.data.items))
-			.catch(error => error);
-	}
+    while (trackCount < PLAYLIST_SIZE) {
+      const randomPlaylist = this.state.selectedPlaylistArray[
+        this.getRandomIntFromZeroToMax(this.state.selectedPlaylistArray.length)
+      ];
+      const randomTrackIndexInPlaylist = this.getRandomIntFromZeroToMax(
+        randomPlaylist.tracksCount
+      );
 
-	setInitialList(items) {
-		let playListArray = items.map(({ id, name, tracks, images }) => ({ id: id, name: name, tracksCount: tracks.total, image: images[0], checked: false }));
-		this.setState({ playListArray });
-	}
+      let playListArray;
+      if (mapPlaylistTrack.has(randomPlaylist.id)) {
+        playListArray = mapPlaylistTrack.get(randomPlaylist.id);
+      } else {
+        playListArray = [];
+        mapPlaylistTrack.set(randomPlaylist.id, playListArray);
+      }
 
-	selectPlayList(playList){
-		playList.checked = !playList.checked;
-		const selectedPlaylistArray = this.state.playListArray.filter(playlist => playlist.checked);
-		this.setState({selectedPlaylistArray});
-	}
+      if (!playListArray.includes(randomTrackIndexInPlaylist)) {
+        playListArray.push(randomTrackIndexInPlaylist);
+        trackCount++;
+      }
+    }
 
+    const promiseArr = [];
+    for (let entry of mapPlaylistTrack) {
+      const playListId = entry[0];
+      for (let trackIndex of entry[1]) {
+        promiseArr.push(this.getPlaylistItem(playListId, trackIndex));
+      }
+    }
 
-	generateRandomizedPlaylist(){
+    axios
+      .all(promiseArr)
+      .then(axios.spread(this.spreadFunction))
+      .catch((error) => error);
+  }
 
-		const mapPlaylistTrack = new Map();
-		let trackCount = 0;
-		
-		while (trackCount < PLAYLIST_SIZE){
-			const randomPlaylist = this.state.selectedPlaylistArray[this.getRandomIntFromZeroToMax(this.state.selectedPlaylistArray.length)];
-			const randomTrackIndexInPlaylist = this.getRandomIntFromZeroToMax(randomPlaylist.tracksCount);
+  spreadFunction(...res) {
+    this.randomTrackList = res.reduce(function (alltracks, thisPlaylisttracks) {
+      alltracks.push(thisPlaylisttracks.data.items[0]);
+      return alltracks;
+    }, []);
 
-			let playListArray;
-			if (mapPlaylistTrack.has(randomPlaylist.id)){
-				playListArray = mapPlaylistTrack.get(randomPlaylist.id);
-			}else{
-				playListArray = [];
-				mapPlaylistTrack.set(randomPlaylist.id ,playListArray );
-			}
+    const randomTrackUriList = this.randomTrackList.map(
+      (item) => item.track.uri
+    );
 
-			if (!playListArray.includes(randomTrackIndexInPlaylist)){
-				playListArray.push( randomTrackIndexInPlaylist );
-				trackCount ++;
-			}
-	
-		}
+    this.playSomeMusic(this.deviceId, randomTrackUriList);
+  }
 
-		const promiseArr = [];
-		for (let entry of mapPlaylistTrack) {
-			const playListId = entry[0];
-			for (let trackIndex of entry[1]){
-				promiseArr.push(this.getPlaylistItem(playListId,trackIndex));
-			}
-		}		
+  getPlaylistItem(playListId, trackIndex) {
+    return axios.get(
+      `https://api.spotify.com/v1/playlists/${playListId}/tracks?limit=1&offset=${trackIndex}`
+    );
+  }
 
-		axios.all(promiseArr)
-		.then(axios.spread(this.spreadFunction))
-		.catch(error => error);
+  getRandomIntFromZeroToMax(maxInt) {
+    return Math.floor(Math.random() * maxInt);
+  }
 
-		
-		
-	}
+  playSomeMusic(deviceid, trackList) {
+    axios
+      .put(`https://api.spotify.com/v1/me/player/play?device_id=${deviceid}`, {
+        uris: trackList,
+      })
+      .then((result) => this.setPlayerState())
+      .catch((error) => error);
+  }
 
-	spreadFunction(...res) {
-		this.randomTrackList = res.reduce( function (alltracks , thisPlaylisttracks) {
-			alltracks.push(thisPlaylisttracks.data.items[0]);		   
-		    return  alltracks; 
-			} , [] );
+  render() {
+    if (!this.state.playListArray) {
+      return null;
+    }
+    const generateButtonEnabled =
+      this.state.selectedPlaylistArray &&
+      this.state.selectedPlaylistArray.length > 0;
 
-		
+    return (
+      <div className="App">
+        <div>
+          <h1>PLAYLIST SCRAMBLER</h1>
+        </div>
+        <div>
+          <div style={{ width: "50%", margin: "0 auto", padding: "20px" }}>
+            <Fade in={generateButtonEnabled}>
+              <Button
+                size="lg"
+                onClick={() => this.generateRandomizedPlaylist()}
+                block
+              >
+                Generate randomized playlist
+              </Button>
+            </Fade>
 
-		const randomTrackUriList = this.randomTrackList.map(item =>  item.track.uri);
-		
+            <Table
+              data={this.state.playListArray}
+              onClickCallBack={this.selectPlayList}
+            />
 
-		this.playSomeMusic(this.deviceId , randomTrackUriList)									  
-	} 
-
-	getPlaylistItem(playListId, trackIndex){
-		return axios.get(`https://api.spotify.com/v1/playlists/${playListId}/tracks?limit=1&offset=${trackIndex}`);
-	}
-
-	
-	getRandomIntFromZeroToMax(maxInt){
-		return Math.floor(Math.random() * (maxInt));
-	}
-	
-  
-	
-	playSomeMusic(deviceid, trackList){
-		axios.put(`https://api.spotify.com/v1/me/player/play?device_id=${deviceid}` , { uris : trackList })
-		.then(result => this.setPlayerState())
-		.catch(error => error);
-	}
-
-
-
-
-
-
-	render() {
-		if (!this.state.playListArray) {
-			return null;
-		}
-		const generateButtonEnabled = this.state.selectedPlaylistArray && this.state.selectedPlaylistArray.length > 0;
-		let button;
-		if(generateButtonEnabled){
-			button = <Button size="lg" onClick={() => this.generateRandomizedPlaylist()} block>Generate randomized playlist</Button>
-		}else{
-			button = <Button size="lg" variant="secondary" disabled block>Generate randomized playlist</Button>
-		}
-
-		return (
-			<div className="App">
-				<div><h1>PLAYLIST SCRAMBLER</h1></div>
-				<div>
-					<div style={{width : '30%' , margin: '0 auto' , padding: '20px'}}> 
-						{button}
-
-						<Table data={this.state.playListArray} onClickCallBack={this.selectPlayList} />
-						
-					<div>
-					</div>
-						<Player playerState={this.state.playerState} randomTrackList={this.randomTrackList} deviceId={this.deviceId}  />				
-					</div>
-				</div>
-			</div>
-			
-
-		)
-
-
-	}
-
-
+            <div></div>
+            <Player
+              playerState={this.state.playerState}
+              randomTrackList={this.randomTrackList}
+              deviceId={this.deviceId}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-
 
 export default App;
--- src/Table.js
+++ src/Table.js
@@ -1,70 +1,77 @@
 import React from "react";
-import Figure from 'react-bootstrap/Figure';
+import Figure from "react-bootstrap/Figure";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function Table({ data, onClickCallBack }) {
+  console.log(data);
 
-	
-
-	const uncheckStyle = {
-		padding: '10px',
-		border: 'solid 1px gray',
-		background: 'papayawhip',
-	}
-
-	const checkStyle = {
-		padding: '10px',
-		border: 'solid 1px gray',
-		background: 'red',
+  const chunck = (array, size) => {
+    const chunked_arr = [];
+    let index = 0;
+    while (index < array.length) {
+      chunked_arr.push(array.slice(index, size + index));
+      index += size;
     }
-    
-    console.log(data);
+    return chunked_arr;
+  };
 
-	return (<div style={{padding: '20px'}}><table style={{ border: 'solid 1px blue', width: '100%' }}>
-		<thead>
-			<tr >
-				<th colSpan='2'
-					style={{
-						borderBottom: 'solid 3px red',
-						background: 'aliceblue',
-						color: 'black',
-						fontWeight: 'bold',
-					}}
-				>
-					Playlist
-	               </th>
-			</tr>
-		</thead>
-		<tbody >
-			{data.map(row => {
-                let image = null;
-                if (row.image){
-                    image =   <Figure>
+  const chunkedPlayListArray = chunck(data, 5);
+
+  console.log(chunkedPlayListArray);
+
+  return (
+    <Container>
+      {chunkedPlayListArray.map((row) => {
+        return (
+          <Row>
+            {row.map((item) => {
+              let imageSrc;
+              if (item.image) {
+                imageSrc = item.image.url;
+              } else {
+                imageSrc = "/spotify-default.jpg";
+              }
+
+              let style;
+              if (item.checked) {
+                style = {
+                  "padding-right": "0px",
+                  "padding-left": "0px",
+                  "border-style": "solid",
+                  "border-width": "15px",
+                  "border-color": "darkkhaki",
+                };
+              } else {
+                style = {
+                  "padding-right": "0px",
+                  "padding-left": "0px",
+                  "border-style": "solid",
+                  "border-width": "15px",
+                  "border-color": "white",
+                };
+              }
+
+              return (
+                <Col key={item.id} style={style}>
+                  <Figure onClick={() => onClickCallBack(item)}>
                     <Figure.Image
                       width={171}
                       height={180}
-                      alt={row.name}
-                      src={row.image.url}
+                      alt="171x180"
+                      src={imageSrc}
                     />
-                    <Figure.Caption>
-                        {row.name}
-                    </Figure.Caption>
-                  </Figure> ;
-                }
-				return (
-					<tr key={row.id} onClick={() => onClickCallBack(row)} style={row.checked ?checkStyle : uncheckStyle }>
-                        <td>
-                            {image}
-                        </td>
-						<td>
-						{row.name}
-						</td>
-					</tr>
-				)
-			})}
-		</tbody>
-	</table></div>
-	)
-
+                    <Figure.Caption>{item.name}</Figure.Caption>
+                  </Figure>
+                </Col>
+              );
+            })}
+          </Row>
+        );
+      })}
+    </Container>
+  );
 }
 
 export default Table;
